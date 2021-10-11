dbb5e951c0bffb8168519f9ea92233ca0e01dfcf
mahmutsbl1 <77998975+mahmutsbl1@users.noreply.github.com>
2021-10-12 02:51:31 +0300

    Add files via upload

---
+++ ilkdeneme.py
@@ -0,0 +1,20 @@
+matematik = input('matematik : ')
+turkce = input('turkce : ')
+kimya = input('kimya : ')
+fizik = input('fizik : ')
+biyoloji = input('biyoloji : ')
+tarih = input('tarih : ')
+toplam=(float(matematik)+float(turkce)+float(kimya)+float(fizik)+float(biyoloji)+float(tarih)*25)/30
+print("Toplam :{0} ".format(toplam))
+if(toplam<50):
+  print("Sınıfta kaldınız")
+else:
+  print("Yaşasın sınıfı geçtin")
+if(toplam<75):
+  print("Yaşasın sınıfı geçtin")
+else:
+  print("Teşşekür Belgesi KAZANDIN")
+if(toplam<85):
+  print("Teşşekür Belgesi KAZANDIN")
+else:
+  print("Taktir Belgesi KAZANDIN")
